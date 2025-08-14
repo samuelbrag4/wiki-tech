42466adf924d1310cf8be754348d945c61c1084d
Fernanda Alves <fernandaalves.tds2.senai@gmail.com>
2025-08-13 23:42:42 -0300

    feat: header start and some refactors

--- src/components/Header/page.jsx
+++ src/components/Header/page.jsx
@@ -1,2 +1,11 @@
-import styles from "./Header.module.css;"
+import React from 'react';
+import Image from 'next/image';
+import styles from "./Header.module.css";
 
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <Image className={styles.logoImg} src="/icons/icons8-configuracao.gif" alt="Logo da Wiki iRice" width={50} height={50} />
+    </header>
+  );
+}
